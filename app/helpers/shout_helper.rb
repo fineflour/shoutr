90340578dc3a3f9ffb54a941a85dfce0c8e88250
fineflour <soojin.cho@bfa.org>
2017-09-26 15:29:38 -0700

    Refactor shout forms

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,4 +1,12 @@
 module ShoutHelper
+  def shout_form_for(content_type)
+    form_for Shout.new do |form| 
+      form.hidden_field(:content_type, value: content_type) + 
+      form.fields_for(:content) { |content_form| yield(content_form) } +
+      form.submit("Shout!")
+    end
+  end
+
   def avatar(user)
     email_digest = Digest::MD5.hexdigest user.email
     gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
